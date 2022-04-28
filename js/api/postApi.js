92b5fd89ba9951322644b44eabda5e440949054c
minhnva910 <fe.minhnva@gmail.com>
2022-04-28 13:44:17 +0700

    adđ interceptor

--- js/api/postApi.js
+++ js/api/postApi.js
@@ -3,23 +3,30 @@ import axiosClient from './axiosClient';
 const postApi = {
   getAll(params) {
     const url = '/posts';
-    return axiosClient.get(url, { params: params });
+    return axiosClient.get(url, { params , baseURL:'htts.com'});
   },
   getById(id) {
     const url = `/posts/${id}`;
     return axiosClient.get(url);
   },
   add(data) {
-      const url ='/posts';
-      return axiosClient.post(url,data);
+    const url = '/posts';
+    return axiosClient.post(url, data);
   },
   update(data) {
-    const url =`/posts/${data.id}`;
-    return axiosClient.patch(url,data);
+    const url = `/posts/${data.id}`;
+    return axiosClient.patch(url, data);
+  },
+  updateFormData(data) {
+    const url = `/posts/${data.id}`;
+    return axiosClient.patch(url, data, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+      // baseURL:
+    });
   },
   remove(id) {
-       const url =`/posts/${id}`;
-    return axiosClient.delete(url,data);
+    const url = `/posts/${id}`;
+    return axiosClient.delete(url, data);
   },
 };
 export default postApi;
